eabbd2d4c45bfc54c09ccb8d4b0520efc7af789d
zakkanativana <zakkanativana13@gmail.com>
2020-12-31 21:19:39 +0700

    File Awal Web Port

---
+++ libraries/My Js/style.js
@@ -0,0 +1,12 @@
+// Animasi Navbar
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
